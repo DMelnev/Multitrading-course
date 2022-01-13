ea63df07bc3f3dd11c74c7df879b77123d1e5f21
DMITRIY <dvmau@mail.ru>
2022-01-13 13:08:13 +0300

    z

--- Concurrency/src/main/java/test.java
+++ Concurrency/src/main/java/test.java
@@ -1,7 +1,67 @@
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class test {
     public static void main(String[] args) {
-       int[] a = new int[10];
-       a[20] = 10;
-       a[5] = a[2] / 0;
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        CountDownLatch count = new CountDownLatch(3);
+        long before = System.currentTimeMillis();
+        executorService.execute(new Runnable() {
+//        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int sum1 = 0;
+                for (int i = 0; i < 1_000_000; i++) {
+                    if (i % 2 == 0) sum1++;
+                }
+                System.out.println("summ of even digits is " + sum1);
+                count.countDown();
+            }
+        });
+//        Thread thread2 = new Thread(new Runnable() {
+                    executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                int sum1 = 0;
+                for (int i = 0; i < 1_000_000; i++) {
+                    if (i % 7 == 0) sum1++;
+                }
+                System.out.println("summ of multiply 7 digits is " + sum1);
+                count.countDown();
+            }
+        });
+//        Thread thread3 = new Thread(new Runnable() {
+                    executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                ArrayList<Integer> list = new ArrayList<>();
+                int sum1 = 0;
+                for (int i = 0; i < 1_000; i++) {
+                    list.add((int) (Math.random() * 100));
+                    if (list.get(i) % 2 == 0) sum1++;
+                }
+                System.out.println("summ of even digits in collection is " + sum1);
+                count.countDown();
+            }
+        });
+//        thread1.start();
+//        thread2.start();
+//        thread3.start();
+        try {
+            count.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        long past = System.currentTimeMillis();
+
+        executorService.shutdown();
+
+        System.out.println("time " + (past - before));
     }
+
+
 }
